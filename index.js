a784eb3c9ce51645558172ebffb450a83204b8b5
ekilar <106618315+ekilar@users.noreply.github.com>
2022-09-04 13:07:17 +0100

    Display forecast with API

--- index.js
+++ index.js
@@ -40,25 +40,40 @@ let date = now.getDate();
 let year = now.getFullYear();
 h2.innerHTML = `${day}, ${date} ${month}, ${year}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-2 mon">
-            <div class="weather-forcast-day">${day}</div>
-            <div>☀️</div>
+            <div class="weather-forcast-day">${formatDay(forecastDay.dt)}</div>
+            <img src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="" width="42"/>
             <div class="weather-forcast-temperatures">
-              <span class="weather-forcast-temperature-max">26°</span>
-              <span class="weather-forcast-temperature-min">20°</span>
+              <span class="weather-forcast-temperature-max">${Math.round(
+                forecastDay.temp.max
+              )}°</span>
+              <span class="weather-forcast-temperature-min">${Math.round(
+                forecastDay.temp.min
+              )}°</span>
             </div>
           </div>     
         `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
